be85c5cecd98fee9c53cd4ebb545f32353e2cdb0
Robert Frank <rob.frank.iii@gmail.com>
2023-08-16 17:13:15 +0000

    contact-list.js day4 completed

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -34,27 +34,61 @@
  */
 
 // YOUR CODE GOES BELOW HERE //
-function makeContact(id, nameFirst, nameLast) {
 
+//a factory function that returns an object with each respective parameter passed into three key-value pairs
+function makeContact(id, nameFirst, nameLast) {
+    return {            //a factory function returns an object.
+        id: id,                     //each parameter in factory function corresponds to a key for contact object
+        nameFirst: nameFirst,
+        nameLast: nameLast
+    };
 } 
 
-
+//factory function that returns an object of methods to manage a contact-list
 function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];  //contacts must be an indexed array for several of the following methods to function properly
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        }, 
+        //method 'addContact' adds a contact object to the contacts list above
+        addContact: function(contact) {
+            contacts.push(contact);
+            return contacts;
+        },
+        //search method 'findContact' uses a for loop to iterate through array and tests each object for strict equality of input parameter 'fullName'
+        findContact: function(fullName) {
+            //uses for loop to test each element of array
+            for (var i = 0; i < contacts.length; i++) {
+                //use template literal to format object key-values into appropriate test for strict equality with parameter passed into method
+                if (fullName === `${contacts[i].nameFirst} ${contacts[i].nameLast}`) {
+                    return contacts[i];
+                }
+            }
+        },
+        //'removeContact' method uses indexOf method of arrays to determine if object is present as an element in contacts
+        removeContact: function(contact) {
+            let idx = contacts.indexOf(contact);
+            if (idx > -1) { //if idx is any value of 0 or greater, that element is removed from array
+                contacts.splice(idx, 1);        //removes 1 element at idx index
+            }
+        },
+        printAllContactNames: function() {
+            let allNameStr = [];    //local variable initialized as empty array to accept strings of full names of each contact
+            //use of empty array allows us to use .push (and later .join) to accept template literals of every full name in contacts.  
+            for (var i = 0; i < contacts.length; i++) {
+                allNameStr.push(`${contacts[i].nameFirst} ${contacts[i].nameLast}`);
+            }
+            return allNameStr.join('\n'); //inserts a new-line character between each element of array
+            //returns a string that only has new line characters between names, not at beginning or end of string.
         }
     }
 }
-
-
-
 
 // YOUR CODE GOES ABOVE HERE //
 
