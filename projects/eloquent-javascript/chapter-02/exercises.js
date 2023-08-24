2f6dfee44e2dc72a88e087a1d250000d5d4b6eae
Robert Frank <rob.frank.iii@gmail.com>
2023-08-24 00:00:27 +0000

    chap2 Eloquent JS complete

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -6,22 +6,45 @@
 
 //takes an input of n to determine how many rows
 function triangles(n) {
-
+	let hash = '';
+	for (var i = 1; i <= n; i++) {
+    	hash += '#';
+      	console.log(hash);
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz() {
-  
+function fizzBuzz(start, end) {
+  for(var i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard(dim) {
+  let board = [];
+  for (let x = 0; x < dim; x++) {
+    let line = '';
+    for (let y = 0; y < dim; y++) {
+      if((x + y) % 2 === 0){line += ' ';}
+      else {line += '#';}
+    }
+    board.push(line);
+  }
+  return board.join('\n');
 }
 
 ////////////////////////////////////////////////////////////////////////////////
