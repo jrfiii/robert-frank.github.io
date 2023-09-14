75372e90788329e91ae38c507e20b7a525a9456c
Robert Frank <rob.frank.iii@gmail.com>
2023-09-14 21:01:33 +0000

    reviewing some recursion prompts

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -143,7 +143,7 @@ var modulo = function(x, y) {
 // ATTENTION DO NOT LEAVE COMMENTS IN THIS FUNCTION. The test is looking for any ('/').
 var multiply = function(x, y) {
   if (x === 0 || y === 0) return 0;
-  if (x === 1 || y === 1) return x;
+  if (x === 1 && y === 1) return 1;
   if (x < 0 && y < 0) {
     return multiply(-x, -y);
   } else if (y < 0) {
